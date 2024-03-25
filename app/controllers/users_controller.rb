a2cb77461257007a190f941c5dcd208f67f23211
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-03-24 23:48:34 -0400

    Finished setting up the backend for forms project

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+class UsersController < ApplicationController # rubocop:disable Layout/EmptyLineAfterMagicComment,Style/Documentation
+  def new; end
+
+  def create
+  end
+end
